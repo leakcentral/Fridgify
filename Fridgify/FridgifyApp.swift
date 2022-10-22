42ccac47791dc875317559ccef49dd111959d3cb
Arnav Patidar <arnav.patidar@gmail.com>
2022-10-22 12:18:29 -0400

    Initial Commit

---
+++ Fridgify/FridgifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgifyApp.swift
+//  Fridgify
+//
+//  Created by Arnav Patidar on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FridgifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
