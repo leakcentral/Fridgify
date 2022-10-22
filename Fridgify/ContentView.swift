30d2c04ec910f680ac5603ce7f7f213131b9782d
Arnav Patidar <arnav.patidar@gmail.com>
2022-10-22 15:01:07 -0400

    UI

--- Fridgify/ContentView.swift
+++ Fridgify/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Fridgify")
-            .fontWeight(.bold)
-            .font(.title)
-            .padding()
+        NavigationView {
+            VStack {
+                ScrollView {
+                    ForEach(0 ..< 15) { item in
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.orange)
+                            .frame(height: 44)
+                            .padding()
+                    }
+                }
+            }
+            .navigationTitle("Fridgify")
+        }
+        .onAppear {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+            appearance.backgroundColor = UIColor(Color.orange.opacity(0.2))
+            
+            // Inline appearance (standard height appearance)
+            UINavigationBar.appearance().standardAppearance = appearance
+            // Large Title appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
     }
 }
 
